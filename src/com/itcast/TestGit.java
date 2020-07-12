50c53a76200db76bbf42fadb0d8f97e1059f00a7
kpwang <kpwang321@163.com>
2020-07-13 00:26:12 +0800

    修改

--- src/com/itcast/TestGit.java
+++ src/com/itcast/TestGit.java
@@ -6,6 +6,7 @@ package com.itcast;
  */
 public class TestGit {
     public static void main(String[] args) {
+        System.out.println("sssssssss");
         System.out.println("ssssssssss");
     }
 }
